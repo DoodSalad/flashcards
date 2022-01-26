874bc1c00bdc2988fb05421db4d56ca0cf358b6d
Riley Hays <haysrg@miamioh.edu>
2022-01-25 23:24:08 -0500

    Added 'next card' button that allows the user receive another random card once they are done studying the current card.

--- routes/cards.js
+++ routes/cards.js
@@ -7,12 +7,16 @@ const cards = data.cards;
 router.get('/', ( req, res ) => {
   const numberOfCards = cards.length;
   const flashcardId = Math.floor( Math.random() * numberOfCards);
-  res.redirect(`/cards/${flashcardId}?side=question`)
+  res.redirect(`/cards/${flashcardId}`)
 });
 
 router.get('/:id', (req, res) => {
   const { side } = req.query;
   const { id } = req.params;
+
+  if(!side){
+    res.redirect(`/cards/${id}?side=question`);
+  }
   const text = cards[id][side];
   const { hint } = cards[id];
 
