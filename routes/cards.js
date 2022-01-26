2142530de6608486ea07ec7c440bbfa2291255bf
Riley Hays <haysrg@miamioh.edu>
2022-01-25 19:22:47 -0500

    Linked the cards to the get method of the cards.js file. Site will now load the cards based on the if passed into the URL

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+// Line 4 and 5 are different ways to accomplish the same thing. ES6 syntax "{ data }" is simply more concise than writing "data = require(../data/flashcardData.json).data"
+const { data } = require('../data/flashcardData.json');
+const cards = data.cards;
 
-router.get('/', (req, res) => {
-  res.render('card', {prompt: 'What is the question?'});
+router.get('/:id', (req, res) => {
+  res.render('card', {
+    prompt: cards[req.params.id].question,
+    hint: cards[req.params.id].hint
+      });
   });
 
   module.exports = router;
